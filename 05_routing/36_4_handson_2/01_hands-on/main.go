31fd6e8fce5ea9b5e47c821cbdb716ad88493173
Allandt Bik-Elliott <allandt@gmail.com>
2024-03-28 13:37:58 +0000

    handson 01 - create tcp server

---
+++ 05_routing/36_4_handson_2/01_hands-on/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Error creating tcp server", err)
+	}
+
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	io.WriteString(conn, "I see you connected\n")
+}
