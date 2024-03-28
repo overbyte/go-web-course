c54b043af242a3aad64fd54cade28476faaeed3e
Allandt Bik-Elliott <allandt@gmail.com>
2024-03-28 15:00:06 +0000

    handson 11 - read from tcp server using bufio - break out of loop to write to browser with go routing and text/plain / http/1.1 headers

---
+++ 05_routing/36_4_handson_2/11_hands-on/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Error creating tcp server", err)
+	}
+
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	s := bufio.NewScanner(conn)
+	for s.Scan() {
+		ln := s.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			break
+		}
+	}
+
+	body := "I see you connected"
+	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/plain\r\n")
+	io.WriteString(conn, "\r\n")
+	io.WriteString(conn, body)
+}
